13cf0f804514608e0f096beb0881fc3b8e01f71e
PauloCK <paulockoff@gmail.com>
2021-01-17 16:29:29 -0300

    Connection with DB

--- api.py
+++ api.py
@@ -1,7 +1,14 @@
+import os
+
 from flask import Flask, request
 from markupsafe import escape
+from dotenv import load_dotenv
 app = Flask(__name__)
+
+load_dotenv()
+print(os.getenv('MONGO_CONN'))
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
