869d7c19025e97b0f341c2d861f02faa965ec732
PauloCK <paulockoff@gmail.com>
2021-01-17 18:14:26 -0300

    Methods POST and GET working in artists endpoint

--- api.py
+++ api.py
@@ -1,14 +1,37 @@
+import json
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from markupsafe import escape
 from dotenv import load_dotenv
+from bson import json_util
+
+from db import insert_artist, get_all_artists
+
 app = Flask(__name__)
 
 load_dotenv()
 print(os.getenv('MONGO_CONN'))
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/artists', methods=['GET','POST'])
+def artists_handler():
+    
+    if request.method == 'POST':
+
+        insert_artist(request.json['artist_name'])
+
+        return 'OK'
+
+    if request.method == 'GET':
+
+        artist_list = get_all_artists()
+        response = {'artists': json.dumps(artist_list,default=json_util.default)}
+    
+        return response
+
 
--- db.py
+++ db.py
@@ -1,30 +1,19 @@
 import os
 
-from dotenv import load_dotenv
 from pymongo import MongoClient
-load_dotenv()
-client = MongoClient(os.getenv('MONGO_CONN'))
 
+client = MongoClient(os.getenv('MONGO_CONN'))
 db = client['spotify']
 artists_collection = db['Artists']
 
-print(os.getenv('MONGO_CONN'))
-# print('\n\n')
-# print(client)
-
 def insert_artist(artist_name):
 
-    #myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     to_insert = {'name': artist_name}
-
-    artist = artists_collection.insert(to_insert)
-    print(artist)
+    artists_collection.insert(to_insert)
+    
 
 def get_all_artists():
 
-    cursor = db['artists_collection'].find({})
-    for item in cursor:
-        print(item)
+    return list(artists_collection.find({}))
 
-get_all_artists()
 
